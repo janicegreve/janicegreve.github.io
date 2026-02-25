317fb2d69db30979b54875e5bd5990b111b69c0d
carsten.greve@gmail.com <carsten.greve@gmail.com>
2026-02-25 11:35:37 +0100

    aalborg sign heart

--- src/components/AalborgSign.jsx
+++ src/components/AalborgSign.jsx
@@ -5,7 +5,7 @@ export const AalborgSign = () => {
     <div className="relative inline-block">
       <img src="/aalborgsignstatic.webp" alt="Book" className="" />
 
-      <motion.img 
+      <motion.img
         src="/aalborgsignwaves.webp"
         style={{
           top: `${826/10}%`,
@@ -22,7 +22,7 @@ export const AalborgSign = () => {
         alt="Moving element"
       />
 
-      <motion.img 
+      <motion.img
         src="/aalborgsignbridge.webp"
         style={{
           top: `${784/10}%`,
@@ -31,15 +31,38 @@ export const AalborgSign = () => {
           transformOrigin: "0 0",
         }}
         className="absolute"
-        animate={{ rotate: [0, -45, -45, 0, 0] }}
+        animate={{ rotate: [-45, -45, 0, 0, -45, -45, 0, 0, -45, -45] }}
         transition={{
-          duration: 10,
+          duration: 20,
           repeat: Infinity,
           ease: "linear",
-          times: [0, 0.2, 0.5, 0.7, 1],
+          times: [0, 0.05, 0.15, 0.25, 0.35, 0.65, 0.75, 0.85, 0.95, 1],
         }}
         alt="Moving element"
       />
-  </div>
+
+      <motion.img
+        src="/aalborgsignheart.webp"
+        style={{
+          top: "20%",
+          left: "5%",
+          width: `${580/30}%`,
+          transformOrigin: "center center",
+        }}
+        className="absolute"
+        animate={{
+          x: ["5%", "5%", "360%", "360%"],
+          rotate: [-45, -45, 45, 45],
+        }}
+        transition={{
+          duration: 10,
+          repeat: Infinity,
+          repeatType: "reverse",
+          ease: "easeInOut",
+          times: [0, 0.2, 0.8, 1],
+        }}
+        alt="Moving element"
+      />
+    </div>
   );
 }
